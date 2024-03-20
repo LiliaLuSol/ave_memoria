0a0c25b7f3218d55a7d195e436a52abc6ae88475
Lilia_LuSol <maria.bochkova.2003.moon@yandex.ru>
2024-03-20 20:28:26 +0300

    add

---
+++ lib/pages/store.dart
@@ -0,0 +1,90 @@
+import 'package:ave_memoria/blocs/Auth/bloc/authentication_bloc.dart';
+import 'package:ave_memoria/main.dart';
+
+import 'package:flutter/material.dart';
+import 'package:ave_memoria/other/app_export.dart';
+
+class Store extends StatefulWidget {
+  const Store({super.key});
+
+  @override
+  State<Store> createState() => _StoreState();
+}
+
+class _StoreState extends State<Store> with TickerProviderStateMixin {
+
+  @override
+  Widget build(BuildContext context) {
+    mediaQueryData = MediaQuery.of(context);
+
+    return BlocListener<AuthenticationBloc, AuthenticationState>(
+      listener: (context, state) {
+        if (state is UnAuthenticatedState) {
+          GoRouter.of(context).pushReplacement(AppRoutes.authreg);
+        } else if (state is AuthErrorState) {
+          context.showsnackbar(title: 'Что-то пошло не так!');
+        }
+      },
+      child: SafeArea(
+        child: Scaffold(
+          extendBody: true,
+          extendBodyBehindAppBar: true,
+          resizeToAvoidBottomInset: false,
+          appBar: CustomAppBar(
+              height: 75.v,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(left: 16.h, right: 16.h),
+                      child: Text("Свод",
+                          style: CustomTextStyles.extraBold32Text)),
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 14.v,
+                      bottom: 9.v,
+                    ),
+                    child: Text(
+                      "0",
+                      style: CustomTextStyles.semiBold18Text,
+                    ),
+                  ),
+                  IconButton(
+                    icon: FaIcon(
+                      FontAwesomeIcons.coins,
+                      size: 25.h,
+                      color: appTheme.yellow,
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              ),
+              styleType: Style.bgFill),
+          body: Container(
+            width: mediaQueryData.size.width,
+            height: mediaQueryData.size.height,
+            child: SizedBox(
+              width: double.maxFinite,
+              child: Column(
+                children: [
+                  SizedBox(height: 75.v),
+                  Divider(height: 1, color: appTheme.gray),
+                  Expanded(
+                      child: Padding(
+                          padding: EdgeInsets.only(left: 16.h, right: 16.h),
+                          child: SingleChildScrollView(
+                              child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [])))),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      //  )
+    );
+  }
+}
