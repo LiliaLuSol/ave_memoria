f933b704a98fe52bf732e3597e26bf4d4cc00100
Lilia_LuSol <maria.bochkova.2003.moon@yandex.ru>
2024-03-25 20:17:19 +0300

    animated

--- lib/pages/base_screen.dart
+++ lib/pages/base_screen.dart
@@ -15,6 +15,7 @@ class BaseScreen extends StatefulWidget {
 
 class _BaseScreenState extends State<BaseScreen> {
   int _selectedIndex = 0;
+  PageController _pageController = PageController(initialPage: 0);
   final List<Widget> _screens = const [
     Homepage(),
     Story(),
@@ -26,83 +27,69 @@ class _BaseScreenState extends State<BaseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          IndexedStack(
-            index: _selectedIndex,
-            children: _screens,
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Column(
-              children: [
-                SizedBox(
-                  width: double.maxFinite,
-                  child: Divider(
-                      height: 1, color: appTheme.gray),
-                ),
-                Container(
-                  height: 89.v,
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.onPrimaryContainer,
-                  ),
-                  child: BottomNavigationBar(
-                    type: BottomNavigationBarType.fixed,
-                    selectedItemColor: theme.colorScheme.primary,
-                    showUnselectedLabels: true,
-                    unselectedItemColor: appTheme.gray,
-                    unselectedLabelStyle: CustomTextStyles.semiBold14Text,
-                    selectedLabelStyle: CustomTextStyles.semiBold14Primary,
-                    backgroundColor: appTheme.white,
-                    currentIndex: _selectedIndex,
-                    onTap: (value) {
-                      setState(() {
-                        _selectedIndex = value;
-                      });
-                    },
-                    items: <BottomNavigationBarItem>[
-                      BottomNavigationBarItem(
-                        icon: FaIcon(
-                          FontAwesomeIcons.buildingColumns,
-                          size: 25.h,
-                        ),
-                        label: 'главная',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: FaIcon(
-                          FontAwesomeIcons.crown,
-                          size: 25.h,
-                        ),
-                        label: 'сюжет',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: FaIcon(
-                          FontAwesomeIcons.feather,
-                          size: 25.h,
-                        ),
-                        label: 'свод',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: FaIcon(
-                          FontAwesomeIcons.chartSimple,
-                          size: 25.h,
-                        ),
-                        label: 'оценка',
-                      ),
-                      BottomNavigationBarItem(
-                        icon: FaIcon(
-                          FontAwesomeIcons.solidUser,
-                          size: 25.h,
-                        ),
-                        label: 'профиль',
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: theme.colorScheme.primary,
+        showUnselectedLabels: true,
+        unselectedItemColor: appTheme.gray,
+        unselectedLabelStyle: CustomTextStyles.semiBold14Text,
+        selectedLabelStyle: CustomTextStyles.semiBold14Primary,
+        backgroundColor: appTheme.white,
+        currentIndex: _selectedIndex,
+        onTap: (value) {
+          setState(() {
+            _selectedIndex = value;
+            _pageController.animateToPage(
+              value,
+              duration: Duration(milliseconds: 300),
+              curve: Curves.ease,
+            );
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.buildingColumns,
+              size: 25.h,
             ),
+            label: 'главная',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.crown,
+              size: 25.h,
+            ),
+            label: 'сюжет',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.feather,
+              size: 25.h,
+            ),
+            label: 'свод',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.chartSimple,
+              size: 25.h,
+            ),
+            label: 'оценка',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(
+              FontAwesomeIcons.solidUser,
+              size: 25.h,
+            ),
+            label: 'профиль',
           ),
         ],
       ),
