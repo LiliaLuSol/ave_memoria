0378cb975e25925784dd27d09ab4793b728ac12e
Lilia_LuSol <maria.bochkova.2003.moon@yandex.ru>
2024-03-26 20:18:24 +0300

    cards game add

---
+++ lib/games/cards_game/utils.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Game {
+  final Color hiddenCard = Colors.red;
+  List<Color>? gameColors;
+  List<String>? gameImg;
+  List<Color> cards = [
+    Colors.green,
+    Colors.yellow,
+    Colors.yellow,
+    Colors.green,
+    Colors.blue,
+    Colors.blue
+  ];
+  final String hiddenCardpath = "";
+  List<String> cards_list = [
+  ];
+  final int cardCount = 8;
+  List<Map<int, String>> matchCheck = [];
+
+  //methods
+  void initGame() {
+    gameColors = List.generate(cardCount, (index) => hiddenCard);
+    gameImg = List.generate(cardCount, (index) => hiddenCardpath);
+  }
+}
