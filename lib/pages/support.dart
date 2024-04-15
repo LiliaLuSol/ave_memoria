1e2cf08b9ed57eafc377bbcb21d9c0d9aba39ded
Lilia_LuSol <maria.bochkova.2003.moon@yandex.ru>
2024-04-15 21:33:14 +0300

    Update support.dart

--- lib/pages/support.dart
+++ lib/pages/support.dart
@@ -29,76 +29,86 @@ class _SupportState extends State<Support> {
     mediaQueryData = MediaQuery.of(context);
 
     return SafeArea(
-        child: Scaffold(
-            extendBody: true,
-            extendBodyBehindAppBar: true,
-            resizeToAvoidBottomInset: false,
-            appBar: CustomAppBar(
-              height: 75.v,
-              leadingWidth: double.maxFinite,
-              leading: Padding(
-                  padding: EdgeInsets.only(left: 20.h),
-                  child: Row(children: [
-                    AppbarIconbutton(
-                        svgPath: ImageConstant.imgArrowleft,
-                        margin: EdgeInsets.only(bottom: 4.v),
-                        onTap: () {
-                          Navigator.pop(context, true);
-                        }),
-                    AppbarSubtitle(
-                        text: "Обратная связь",
-                        margin: EdgeInsets.only(left: 16.h),)
-                  ])),
-              styleType: Style.bgFill,
-            ),
-            body: Container(
-                width: mediaQueryData.size.width,
-                height: mediaQueryData.size.height,
-                child: SizedBox(
-                    width: double.maxFinite,
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(
-                            height: 75.v,
-                          ),
-                          Divider(height: 1, color: appTheme.gray),
-                          Padding(
-                              padding: EdgeInsets.only(
-                                  left: 20.h, right: 20.h, top: 16.h),
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text("Ваше сообщение",
-                                        style: theme.textTheme.bodyMedium),
-                                    SizedBox(height: 4.v),
-                                    SingleChildScrollView(
-                                        child: CustomTextFormField(
-                                            controller:
-                                                messageplaceholController,
-                                            focusNode: msgFocusNode,
-                                            maxLines: 30,
-                                            autofocus: false,
-                                            hintText: "Сообщение...",
-                                            textInputAction:
-                                                TextInputAction.done)),
-                                    SizedBox(height: 16.v),
-                                    CustomElevatedButton(
-                                      text: "Оправить",
-                                      buttonStyle: ElevatedButton.styleFrom(
-                                        backgroundColor:
-                                            theme.colorScheme.primary,
-                                      ),
-                                      onTap: () {
-                                        context.showsnackbar(
-                                            title: 'Сообщение отправлено!',
-                                            color: Colors.grey);
-                                        GoRouter.of(context)
-                                            .push(AppRoutes.homepage);
-                                      },
-                                      alignment: Alignment.center,
-                                    ),
-                                  ]))
-                        ])))));
+        child: GestureDetector(
+            onTap: () {
+              FocusScope.of(context).unfocus();
+            },
+            child: Scaffold(
+                extendBody: true,
+                extendBodyBehindAppBar: true,
+                resizeToAvoidBottomInset: false,
+                appBar: CustomAppBar(
+                  height: 75.v,
+                  leadingWidth: double.maxFinite,
+                  leading: Padding(
+                      padding: EdgeInsets.only(left: 20.h),
+                      child: Row(children: [
+                        AppbarIconbutton(
+                            svgPath: ImageConstant.imgArrowleft,
+                            margin: EdgeInsets.only(bottom: 4.v),
+                            onTap: () {
+                              Navigator.pop(context, true);
+                            }),
+                        AppbarSubtitle(
+                          text: "Обратная связь",
+                          margin: EdgeInsets.only(left: 16.h),
+                        )
+                      ])),
+                  styleType: Style.bgFill,
+                ),
+                body: Container(
+                    width: mediaQueryData.size.width,
+                    height: mediaQueryData.size.height,
+                    child: SizedBox(
+                        width: double.maxFinite,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                height: 75.v,
+                              ),
+                              Divider(height: 1, color: appTheme.gray),
+                              Padding(
+                                  padding: EdgeInsets.only(
+                                      left: 20.h, right: 20.h, top: 16.h),
+                                  child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text("Ваше сообщение",
+                                            style: theme.textTheme.bodyMedium),
+                                        SizedBox(height: 4.v),
+                                        SingleChildScrollView(
+                                            child: CustomTextFormField(
+                                                controller:
+                                                    messageplaceholController,
+                                                focusNode: msgFocusNode,
+                                                maxLines: 19,
+                                                autofocus: false,
+                                                hintText: "Сообщение...",
+                                                textInputAction:
+                                                    TextInputAction.done)),
+                                        SizedBox(height: 100.v),
+                                        CustomElevatedButton(
+                                          text: "Отправить",
+                                          buttonTextStyle: CustomTextStyles
+                                              .semiBold18TextWhite,
+                                          buttonStyle: ElevatedButton.styleFrom(
+                                              backgroundColor:
+                                                  theme.colorScheme.primary,
+                                              shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(
+                                                          5))),
+                                          onTap: () {
+                                            context.showsnackbar(
+                                                title: 'Сообщение отправлено!',
+                                                color: Colors.grey);
+                                            GoRouter.of(context)
+                                                .push(AppRoutes.homepage);
+                                          },
+                                        ),
+                                      ]))
+                            ]))))));
   }
 }
