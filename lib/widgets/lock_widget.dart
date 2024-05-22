b8ad72db8e2ba1bd5cb804350cdf49bc0d88e9fb
Lilia_LuSol <maria.bochkova.2003.moon@yandex.ru>
2024-05-23 01:00:31 +0300

    update

--- lib/widgets/lock_widget.dart
+++ lib/widgets/lock_widget.dart
@@ -11,8 +11,20 @@ Widget lock() {
       ),
       Padding(
           padding: EdgeInsets.symmetric(horizontal: 16.h),
-          child: Text('Ограниченный доступ для анонимного пользователя',
+          child: Text('Ограниченный доступ для анонимного пользователя!',
               style: CustomTextStyles.semiBold18TextGray,
+              textAlign: TextAlign.center)),
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.h),
+          child: Text(
+              'В этом режиме Вам недоступны сетевые функции, но мини-игры всегда доступны',
+              style: CustomTextStyles.regular16Text,
+              textAlign: TextAlign.center)),
+      SizedBox(height: 8.v),
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.h),
+          child: Text('¯\\_(ツ)_/¯',
+              style: CustomTextStyles.regular16Text,
               textAlign: TextAlign.center)),
       const Spacer(),
     ],
--- lib/widgets/no_internet_widget.dart
+++ lib/widgets/no_internet_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ave_memoria/other/app_export.dart';
 
 Widget no_internet() {
   return Column(
@@ -8,6 +9,23 @@ Widget no_internet() {
       Image.asset(
         'assets/images/no-internet.png',
       ),
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.h),
+          child: Text('О нет! Великий интернет пропал!',
+              style: CustomTextStyles.semiBold18TextGray,
+              textAlign: TextAlign.center)),
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.h),
+          child: Text(
+              'Вы можете продолжать играть в мини-игры, но мы не можем гарантировать сохраность новых рекордов и денег',
+              style: CustomTextStyles.regular16Text,
+              textAlign: TextAlign.center)),
+      SizedBox(height: 8.v),
+      Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.h),
+          child: Text('＞︿＜',
+              style: CustomTextStyles.regular16Text,
+              textAlign: TextAlign.center)),
       const Spacer(),
     ],
   );
